5839adac10dd2d5da7eb8b4d0ffb2c84f1af15e5
thierrysio <thierry28220@icloud.com>
2025-09-15 10:34:27 +0200

    ok

--- Voiliers2/Vues/VueLocation.xaml.cs
+++ Voiliers2/Vues/VueLocation.xaml.cs
@@ -1,13 +1,19 @@
+using System.Collections.ObjectModel;
 using Voiliers2.Modeles;
 
 namespace Voiliers2.Vues;
 
 public partial class VueLocation : ContentPage
 {
-	public VueLocation()
+    public ObservableCollection<Voilier> mesVoiliers { get; set; } 
+		= new ObservableCollection<Voilier>();
+    public VueLocation()
 	{
 		InitializeComponent();
-		this.afficherCapacite(new Voilier(5));
+        BindingContext = this;
+
+        this.afficherCapacite(new Voilier(5));
+		this.afficherVoiliers();
 
 	}
 	private void afficherCapacite(Voilier unVoilier)
@@ -15,4 +21,13 @@ public partial class VueLocation : ContentPage
 		nbCapacite.Text = unVoilier.Capacite.ToString();
 
     }
+
+	private void afficherVoiliers()
+	{
+		for (int i = 1; i < 6; i++)
+		{
+			mesVoiliers.Add(new Voilier(i));
+		}
+
+	}
 }
